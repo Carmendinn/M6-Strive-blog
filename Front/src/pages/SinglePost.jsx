0225687cc44b785ab679b64aac2681c27b9ce6be
Carmendinn <dinnocenzocarmen@gmail.com>
2024-07-25 14:06:04 +0200

    commit 9

--- Front/src/pages/SinglePost.jsx
+++ Front/src/pages/SinglePost.jsx
@@ -173,88 +173,110 @@ export default function PostDetail() {
   if (!post) return <div>Caricamento...</div>;
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <article className="bg-white shadow-md rounded-lg overflow-hidden">
-        <header className="relative">
-          <img src={post.cover} alt={post.title} className="w-full h-64 object-cover" />
-          <div className="text-white">
-            <h1 className="text-3xl font-bold mb-2">{post.title}</h1>
-            <div className="text-sm">
-              <span className="block">Categoria: {post.category}</span>
-              <span className="block">Autore: {post.author}</span>
-              <span className="block">Tempo di lettura: {post.readTime.value} {post.readTime.unit}</span>
-            </div>
+    <div className="container mx-auto p-4">
+      <article className="bg-white rounded-lg shadow-md p-6">
+        <img
+          src={post.cover}
+          alt={post.title}
+          className="w-full h-64 object-cover rounded-t-lg"
+        />
+        <h1 className="text-3xl font-bold mt-4 mb-2">{post.title}</h1>
+        <div className="text-gray-600 mb-4">
+          <span className="block">Categoria: {post.category}</span>
+          <span className="block">Autore: {post.author}</span>
+          <span className="block">
+            Tempo di lettura: {post.readTime.value} {post.readTime.unit}
+          </span>
+        </div>
+        <div
+          className="text-gray-800 leading-relaxed mb-4"
+          dangerouslySetInnerHTML={{ __html: post.content }}
+        />
+        {isLoggedIn && (
+          <div className="mt-4">
+            <button
+              onClick={handleEditPost}
+              className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2"
+            >
+              Modifica Post
+            </button>
+            <button
+              onClick={handleDeletePost}
+              className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Elimina Post
+            </button>
           </div>
-        </header>
-        <section className="p-4">
-          {/* Sezione Contenuto del Post */}
-          <div
-            className="post-text mb-4"
-            dangerouslySetInnerHTML={{ __html: post.content }}
-          />
-
-          {/* Sezione Metadati del Post */}
-          <div className="text-white">
-            <h1 className="text-3xl font-bold mb-2">{post.title}</h1>
-            <div className="text-sm">
-              <span className="block">Categoria: {post.category}</span>
-              <span className="block">Autore: {post.author}</span>
-              <span className="block">Tempo di lettura: {post.readTime.value} {post.readTime.unit}</span>
-            </div>
-          </div>
-        </section>
-        <section className="p-4 border-t border-gray-200">
-          <h3 className="text-2xl font-semibold mb-4">Commenti</h3>
-          {comments.length > 0 ? (
-            <div className="space-y-4">
-              {comments.map((comment) => (
-                <div key={comment._id} className="bg-gray-100 p-4 rounded-lg">
-                  {editingComment === comment._id ? (
-                    <div className="flex flex-col space-y-2">
-                      <textarea
-                        value={editedCommentContent}
-                        onChange={(e) => setEditedCommentContent(e.target.value)}
-                        className="w-full p-2 border border-gray-300 rounded-md"
-                      />
-                      <div className="flex space-x-2">
-                        <button onClick={() => handleSaveEdit(comment._id)} className="bg-blue-500 text-white px-4 py-2 rounded-md">Salva</button>
-                        <button onClick={() => setEditingComment(null)} className="bg-gray-500 text-white px-4 py-2 rounded-md">Annulla</button>
-                      </div>
-                    </div>
-                  ) : (
-                    <div>
-                      <p className="text-gray-800 mb-2">{comment.content}</p>
-                      <div className="text-sm text-gray-600">
-                        <small>Di: {comment.name}</small>
-                        {userData && userData.email === comment.email && (
-                          <div className="mt-2 flex space-x-2">
-                            <button onClick={() => handleEditComment(comment._id, comment.content)} className="bg-yellow-500 text-white px-4 py-2 rounded-md">Modifica</button>
-                            <button onClick={() => handleDeleteComment(comment._id)} className="bg-red-500 text-white px-4 py-2 rounded-md">Elimina</button>
-                          </div>
-                        )}
-                      </div>
-                    </div>
-                  )}
-                </div>
-              ))}
-            </div>
-          ) : (
-            <p className="text-gray-500">Non ci sono ancora commenti per questo post.</p>
-          )}
-          {isLoggedIn && (
-            <form onSubmit={handleCommentSubmit} className="mt-4 bg-gray-100 p-4 rounded-lg">
-              <h4 className="text-lg font-semibold mb-2">Aggiungi un commento</h4>
-              <textarea
-                value={newComment.content}
-                onChange={(e) => setNewComment({ ...newComment, content: e.target.value })}
-                placeholder="Scrivi il tuo commento..."
-                className="w-full p-2 border border-gray-300 rounded-md mb-2"
-              />
-              <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md">Invia commento</button>
-            </form>
-          )}
-        </section>
+        )}
+        {editingPost && (
+          <form onSubmit={handleEditPostSubmit} className="mt-4">
+            <textarea
+              value={editedContent}
+              onChange={(e) => setEditedContent(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded"
+              rows="6"
+            />
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-2"
+            >
+              Salva Modifiche
+            </button>
+          </form>
+        )}
       </article>
-    </div >
+      
+      <section className="p-4 border-t border-gray-200">
+        <h3 className="text-2xl font-semibold mb-4">Commenti</h3>
+        {comments.length > 0 ? (
+          <div className="space-y-4">
+            {comments.map((comment) => (
+              <div key={comment._id} className="bg-gray-100 p-4 rounded-lg">
+                {editingComment === comment._id ? (
+                  <div className="flex flex-col space-y-2">
+                    <textarea
+                      value={editedCommentContent}
+                      onChange={(e) => setEditedCommentContent(e.target.value)}
+                      className="w-full p-2 border border-gray-300 rounded-md"
+                    />
+                    <div className="flex space-x-2">
+                      <button onClick={() => handleSaveEdit(comment._id)} className="bg-blue-500 text-white px-4 py-2 rounded-md">Salva</button>
+                      <button onClick={() => setEditingComment(null)} className="bg-gray-500 text-white px-4 py-2 rounded-md">Annulla</button>
+                    </div>
+                  </div>
+                ) : (
+                  <div>
+                    <p className="text-gray-800 mb-2">{comment.content}</p>
+                    <div className="text-sm text-gray-600">
+                      <small>Di: {comment.name}</small>
+                      {userData && userData.email === comment.email && (
+                        <div className="mt-2 flex space-x-2">
+                          <button onClick={() => handleEditComment(comment._id, comment.content)} className="bg-yellow-500 text-white px-4 py-2 rounded-md">Modifica</button>
+                          <button onClick={() => handleDeleteComment(comment._id)} className="bg-red-500 text-white px-4 py-2 rounded-md">Elimina</button>
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p className="text-gray-500">Non ci sono ancora commenti per questo post.</p>
+        )}
+        {isLoggedIn && (
+          <form onSubmit={handleCommentSubmit} className="mt-4 bg-gray-100 p-4 rounded-lg">
+            <h4 className="text-lg font-semibold mb-2">Aggiungi un commento</h4>
+            <textarea
+              value={newComment.content}
+              onChange={(e) => setNewComment({ ...newComment, content: e.target.value })}
+              placeholder="Scrivi il tuo commento..."
+              className="w-full p-2 border border-gray-300 rounded-md mb-2"
+            />
+            <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md">Invia commento</button>
+          </form>
+        )}
+      </section>
+    </div>
   );
-};
+}
